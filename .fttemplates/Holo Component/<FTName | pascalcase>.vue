92d4e9e5f7e570fd6952594d1dff0151d97ad7ee
OviOvocny <werint.pp@gmail.com>
2022-06-12 19:35:16 +0200

    fttemplates

---
+++ .fttemplates/Holo Component/<FTName | pascalcase>.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  name: 'H<FTName | pascalcase>'
+}
+</script>
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+  
+</template>
+
+<style>
+.holo-<FTName | lowercase> {
+  
+}
+</style>
---
+++ .fttemplates/Holo Component/index.ts
@@ -0,0 +1,8 @@
+import { App } from 'vue'
+import <FTName | pascalcase> from './<FTName | pascalcase>.vue'
+
+<FTName | pascalcase>.install = (Vue: App) => {
+  Vue.component(<FTName | pascalcase>.name, <FTName | pascalcase>)
+}
+
+export default <FTName | pascalcase>
