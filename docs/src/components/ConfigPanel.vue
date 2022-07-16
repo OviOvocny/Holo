209ed4be60c800cf0daf2031332274c7dfdf7b38
OviOvocny <werint.pp@gmail.com>
2022-07-16 22:58:49 +0200

    docs: add resulting tag view to configurator

--- docs/src/components/ConfigPanel.vue
+++ docs/src/components/ConfigPanel.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { ComponentProp } from '@docs/types/ComponentDocumentation'
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
+import { useClipboard } from '@vueuse/core'
 
 const props = defineProps<{
+  name: string
   properties: ComponentProp[]
 }>()
 
-interface Config {
-  [name: string]: any
-}
+type Config = Record<string, string | number | boolean | object>
 
 function fallback(propType: string) {
   switch (propType) {
@@ -43,13 +43,51 @@ const defaultProps: Config = props.properties.reduce(
 const config = reactive<Config>({
   ...defaultProps
 })
+
+const outputTag = computed(() => {
+  const { name, properties } = props
+  const configProps = Object.entries(config)
+    .filter(([, value]) => value !== null)
+    .map(([name, value]) => {
+      const prop = properties.find((prop) => prop.name === name)
+      if (!prop) {
+        return null
+      }
+      const { type, default: defaultValue } = prop
+      if (defaultValue && sanitize(defaultValue) === value) {
+        return null
+      }
+      switch (type) {
+        case 'boolean':
+          return value ? name : null
+        case 'number':
+          return `:${name}="${value}"`
+        case 'string':
+          return `${name}="${value}"`
+        case 'object':
+        case 'array':
+          return `:${name}="${JSON.stringify(value)}"`
+        default:
+          return null
+      }
+    })
+    .join(' ')
+    .trim()
+  const openingTag = `${name} ${configProps}`.trim()
+  return `<${openingTag}></${name}>`
+})
+
+const { copy, isSupported: clipboardSupported } = useClipboard({
+  source: outputTag
+})
 </script>
 
 <template>
   <AGrid
     class="config-container"
     template="
-    '  display controls' auto
+    '  display                   controls' auto
+    '  output                    output  ' max-content
       / minmax(max-content, 1fr) auto"
   >
     <Area area="display">
@@ -78,6 +116,21 @@ const config = reactive<Config>({
           />
         </ListItem>
       </List>
+    </Area>
+    <Area area="output">
+      <div class="config-output-tag">
+        <code class="mono-font">{{ outputTag }}</code>
+        <Button
+          v-show="clipboardSupported"
+          color="accent"
+          hollow
+          borderless
+          symmetrical
+          @click="copy()"
+        >
+          <Icon name="ui-clipboard" />
+        </Button>
+      </div>
     </Area>
   </AGrid>
 </template>
@@ -108,5 +161,15 @@ const config = reactive<Config>({
 
 .config-input {
   margin: 0;
+}
+
+.config-output-tag {
+  background-color: hsl(var(--background));
+  padding-block: 0.25em;
+  padding-inline-start: 1em;
+  margin-block-start: 1em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
